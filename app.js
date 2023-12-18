97aa9817dbb59fde90d1b2095f19ac037e22a4ed
Sanjay Nithin <sanjayfornewsletter@gmail.com>
2023-12-19 03:31:34 +0530

    handling errors

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use('/', userRoutes);
 // handling errors
 app.use((req, res, next) => {
     const error = new Error('Not Found');
-    error.status(404);
+    error.status= 404;
     next(error);
 });
 
