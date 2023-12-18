875e561999c2583fae9f5aa7cfc8c3463b987f5c
Sanjay Nithin <sanjayfornewsletter@gmail.com>
2023-12-19 02:22:39 +0530

    add base route

--- api/routes/Users.js
+++ api/routes/Users.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const userControllers = require('../controllers/Users');
 
 // handling signup and login requests.
+router.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome Accredian-Backend-Task Web Server'
+    });
+});
 router.post('/signup', userControllers.signup);
 router.post('/login', userControllers.login);
 
