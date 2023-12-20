20fb53c73ff9da71aa29e7065f28f6822b20c103
Sanjay Nithin <sanjayfornewsletter@gmail.com>
2023-12-20 16:47:44 +0530

    handling cors again

--- api/routes/Users.js
+++ api/routes/Users.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userControllers = require('../controllers/Users');
-
+const cors = require('cors');
 // handling signup and login requests.
+router.use(cors());
 router.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome Accredian-Backend-Task Web Server'
